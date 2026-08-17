7df16a27120a12b9fb062fb5d5069886438aa805
renovaport <renovaport@gmail.com>
2026-08-17 18:46:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE="renovaport-v2";
+const CACHE="renovaport-v3";
 const FILES=["./","./index.html","./styles.css","./manifest.webmanifest","./logo-renovaport.png","./logo-192.png","./logo-512.png","./favicon.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
